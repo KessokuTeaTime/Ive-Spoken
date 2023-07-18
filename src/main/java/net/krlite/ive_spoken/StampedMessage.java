0b396e7105c7be4c2fb7fc03c734bf0796534f27
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-18 12:57:21 +0800

    artwork

--- src/main/java/net/krlite/ive_spoken/StampedMessage.java
+++
@@ -1,9 +0,0 @@
-package net.krlite.ive_spoken;
-
-import net.minecraft.text.Text;
-
-public record StampedMessage(long timestamp, Text message) {
-	public boolean expired() {
-		return System.currentTimeMillis() - timestamp() >= IveSpoken.CONFIG.lastingTime();
-	}
-}
